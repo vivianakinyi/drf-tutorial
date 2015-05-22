b15a675300f1479e5c997486eb894522c3dfd490
Akinyi <vivianmondi@gmail.com>
2015-05-22 23:40:40 +0300

    Added endpoints to our users.
    Modified the views.py to add the new user generic views.
    Also modifies snippets/url.py to point to the new user url.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,5 +1,6 @@
 from django.forms import widgets
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from snippets.models import Snippet , LANGUAGE_CHOICES, STYLE_CHOICES
 
 #refactored code
@@ -7,6 +8,13 @@ class SnippetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Snippet
 		fields =('id', 'title', 'code', 'linenos', 'language', 'style')
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
 
 
 
