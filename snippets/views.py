eeac8df47ae451e442c66c7dac8eac5e38b37863
Akinyi <vivianmondi@gmail.com>
2015-05-22 21:56:38 +0300

    Authentications and permissions: Added a field in model to represent the user

--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,37 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+
+#Class based views with mixins
+
+# from snippets.models import Snippet
+# from snippets.serializers import SnippetSerializer
+# from rest_framework import mixins
+# from rest_framework import generics
+
+# class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
+
+# 	def put(self, request, *args, **kwargs):
+# 		return self.update(request, *args, **kwargs)
+
+# 	def delete(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)
+
+
 #Use of mixins to reuse
 
 
