e32af8bc05366bb20e96fcfe69a93b470e258066
Akinyi <vivianmondi@gmail.com>
2015-05-22 09:39:40 +0300

    Change to the project tutorial and app snippet
    Had started on the quickstart tutorial instead of snippet.Hence change to use the snippet app but under the same project tutorial.

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
