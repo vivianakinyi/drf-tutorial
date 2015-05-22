3b315b8fe3c7071ce38f9296304ab0ecd1e4aeb9
Akinyi <vivianmondi@gmail.com>
2015-05-22 23:57:06 +0300

    Added required permissions to views.
    This is used to determine if user is read-only or has read-write access.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,7 +7,8 @@ from snippets.models import Snippet , LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Snippet
-		fields =('id', 'title', 'code', 'linenos', 'language', 'style')
+		fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+		owner = serializers.ReadOnlyField(source='owner.username')
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
--- snippets/views.py
+++ snippets/views.py
@@ -3,11 +3,15 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
+from rest_framework import permissions
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    #Added a .perform_create() method to associate snippet with users.
     def perform_create(self, serializer):
     	serializer.save(owner=self.request.user)
 
@@ -15,6 +19,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
