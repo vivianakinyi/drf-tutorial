6020c5efa313beb805b583f0507aef60ae7533a8
Akinyi <vivianmondi@gmail.com>
2015-05-22 17:10:31 +0300

    Class based views Modified the views to use classes. Same as function based only that its allows reuse of functionalities.

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,9 +2,17 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
+#urls for class based views
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
+
+#urls for function based views
+
+# urlpatterns = [
+#     url(r'^snippets/$', views.snippet_list),
+#     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+# ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
