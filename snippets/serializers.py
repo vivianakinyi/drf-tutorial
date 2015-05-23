f49e69bedba6712f2559441fb94eea09517e4a54
Akinyi <vivianmondi@gmail.com>
2015-05-24 00:54:02 +0300

    Modified urls.py to have hyperlinked API Also included paginations in settings.py

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -17,7 +17,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'snippets')
 
+#Hyperlinkied style between entitities
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
 
 	#Un-refactored code
 # class SnippetSerializer(serializers.Serializer):
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -106,3 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Added Pagination
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
