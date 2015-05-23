b95d22d3bb0d344da10b941215d0babf07f7f865
Akinyi <vivianmondi@gmail.com>
2015-05-23 17:39:04 +0300

    Created endpointf for the highlighted snippets.
    Use of HTML representation.Pre-rendered HTML.

--- snippets/views.py
+++ snippets/views.py
@@ -4,13 +4,27 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+
+
+#Creating an endpoint for the root of our API
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
     #Added a .perform_create() method to associate snippet with users.
     def perform_create(self, serializer):
     	serializer.save(owner=self.request.user)
@@ -30,7 +44,14 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+#Endpoints for the highlighted snippets
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
 
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 #Class based views with mixins
 
